75afa1817fdd7715838d2b9e4716a13d64525828
Alex Neil <alex.neil@60decibels.com>
2021-07-19 20:35:53 +0500

    updated const vars

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,5 @@
-export const API = 'https://p3deecn8ff.execute-api.eu-west-2.amazonaws.com/dev/genflow-tech-test-function-v2';
+const API_KEY = 'p3deecn8ff';
+const API_REGION = 'eu-west-2';
+const API_PATH = 'dev/genflow-tech-test-function-v2';
+export const API = `https://${API_KEY}.execute-api.${API_REGION}.amazonaws.com/${API_PATH}`;
 export const ITEM_ID = '1';
